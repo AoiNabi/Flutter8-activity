9939de7dc7d3c71ef94eb8bce2afe0c37a30815e
AoiNabi <matthew.pariona@unmsm.edu.pe>
2026-05-30 09:39:02 -0500

    cambios de estructura

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def create_app():
 
 app = create_app()
 
+init_db()
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 # config.py - Configuración general del backend
 import os
-
+import tempfile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 class Config:
     """Configuración base de la aplicación Flask."""
     SECRET_KEY = 'techstore_secret_2026'
-    DATABASE_PATH = os.path.join(BASE_DIR, 'database.db')
+    
+    if os.environ.get('RENDER'):
+        DATABASE_PATH = os.path.join('/tmp', 'database.db')
+    else:
+        DATABASE_PATH = os.path.join(BASE_DIR, 'database.db')
